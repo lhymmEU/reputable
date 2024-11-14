00652b3784e5f37a398fa6e083515305f1683a91
lhymmEU <italy.18429@gmail.com>
2024-11-14 18:41:16 +0700

    beautified name tag

--- app/components/Action/index.tsx
+++ app/components/Action/index.tsx
@@ -13,12 +13,16 @@ export default function Action() {
 
   const handleCreateRep = () => {
     router.push("/reputation/create");
-  }
+  };
 
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col items-center p-4">
       <h1 className="text-3xl font-bold text-gray-800 mb-8 mt-12">Reputable</h1>
-      {userData && <h1 className="text-3xl font-bold text-gray-800 mb-8 mt-12">{userData.username}</h1>}
+      {userData && (
+        <h1 className="text-lg text-center font-semibold text-white bg-black px-4 py-2 rounded-full shadow-md mb-8 mt-2 w-full">
+          {userData.username}
+        </h1>
+      )}
 
       <div className="w-full max-w-md space-y-6" onClick={handleCreateRep}>
         <Card className="bg-gradient-to-br from-blue-500 to-purple-600 text-white rounded-xl overflow-hidden transition-all duration-300 hover:shadow-xl">
